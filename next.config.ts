d49f5eff7204b4e0a8797fa3ed9dc0b7757188b7
Masai Mouzon <mightymasai@gmail.com>
2025-09-21 13:46:42 -0700

    fix: Corrected next.config.ts to disable lightningcss

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from 'next';
 
-const nextConfig NextConfig = {
-  experimental {
-     This flag disables the use of lightningcss for CSS processing.
-    lightningCss false,
+const nextConfig: NextConfig = {
+  experimental: {
+    // This flag disables the use of lightningcss for CSS processing.
+    lightningCss: false,
   },
 };
 
